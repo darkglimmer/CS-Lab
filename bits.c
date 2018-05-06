2c5c9208c61c717c9faa462e3b968a182b383ea4
darkglimmer <2392897209@qq.com>
2018-05-06 15:08:07 +0800

    bits

--- bits.c
+++ bits.c
@@ -173,13 +173,12 @@ int logicalShift(int x, int n) {
  *   Rating: 4
  */
 int bitCount(int x) {
-    int sum = 0;
-    int j = 1;
-    while(j <= (1 << 31)){
-        sum += x & j;
-        j << 1;
-    }
-    return sum;
+    x = (x & 0x55555555) + ((x >> 1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+    x = (x & 0x0f0f0f0f) + ((x >> 4) & 0x0f0f0f0f);
+    x = (x & 0x00ff00ff) + ((x >> 8) & 0x00ff00ff);
+    x = (x & 0x0000ffff) + ((x >> 16) & 0x0000ffff);
+    return x;
 }
 /* 
  * bang - Compute !x without using !
